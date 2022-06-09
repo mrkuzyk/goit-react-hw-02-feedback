520b19b46d1abb9db2aedcfd563c73f1726bb7d8
Marian Kuzyk <mariankuzyk@gmail.com>
2022-06-09 23:46:52 +0300

    render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,29 @@ import { Component } from 'react';
 
 
 export class App extends Component {
+  
+  state = {
+    good: 1,
+    neutral: 2,
+    bad: 3
+  }
   render() {
+    const { good, neutral, bad } = this.state;
     return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <div>
+        <h1>Please leave feedback</h1>
+        <ul>
+          {Object.keys(this.state).map(key => <button key={key}>{ key}</button>)}
+        </ul>
+        <h1>Statistics</h1>
+        <ul>
+          <li>Good : {good} </li>
+          <li>Neutral : {neutral} </li>
+          <li>Bad : {bad} </li>
+          <li>Total: {bad + good + neutral} </li>
+          <li>Bad : {bad} </li>
+        </ul>
+      </div>
   );
   }
 };
